fbcf021cbbedfdd970084dbd083f3ba47a42b18b
unknown <yy.ainu@gmail.com>
2020-09-20 17:55:56 +0300

    Updates

---
+++ precache-manifest.478eaaaad33ab910ff26fc6830e1fdd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45334668c684c1002cb1990940cb83a",
+    "url": "/yygthb/compare_prices/index.html"
+  },
+  {
+    "revision": "6fd622f36fd1c3f6a28a",
+    "url": "/yygthb/compare_prices/static/css/main.255bafb2.chunk.css"
+  },
+  {
+    "revision": "f8c3202dfb5b3a573e76",
+    "url": "/yygthb/compare_prices/static/js/2.f858dd09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yygthb/compare_prices/static/js/2.f858dd09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd622f36fd1c3f6a28a",
+    "url": "/yygthb/compare_prices/static/js/main.fad9f91d.chunk.js"
+  },
+  {
+    "revision": "b17dc16c0362dfc52d48",
+    "url": "/yygthb/compare_prices/static/js/runtime-main.e0eab8b2.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/yygthb/compare_prices/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "f87b10ceb29557594395a464b34738ac",
+    "url": "/yygthb/compare_prices/static/media/image_info2.f87b10ce.jpg"
+  }
+]);
